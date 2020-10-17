a446c458fefa21ff7d6f4b70e5c80220621b97d0
NichoSeb2 <NichoSeb2@outlook.fr>
2020-10-17 16:46:53 +0200

    multiple SELECT

--- src/SQL.js
+++ src/SQL.js
@@ -7,12 +7,12 @@ function buildSQL_wheres(wheres, escape)
 		if(typeof wheres === "object" && wheres.length === undefined)
 		{
 			sql.push("WHERE");
-			sql.push([escape + wheres.field + escape, wheres.operator, (typeof wheres.value === "number" ? wheres.value : "\"" + wheres.value + "\"")].join(" "));
+			sql.push([escape + wheres.field + escape, wheres.operator, (typeof wheres.value === "number" ? wheres.value : (wheres.valueEscape !== undefined ? wheres.valueEscape : "\"") + wheres.value + (wheres.valueEscape !== undefined ? wheres.valueEscape : "\""))].join(" "));
 		}
 		else if(typeof wheres === "object" && wheres.length !== undefined)
 		{
 			sql.push("WHERE");
-			sql.push(wheres.map((where) => { return [escape + where.field + escape, where.operator, (typeof where.value === "number" ? where.value : "\"" + where.value + "\"")].join(" "); }).join(" AND "));
+			sql.push(wheres.map((where) => { return [escape + where.field + escape, where.operator, (typeof where.value === "number" ? where.value : (where.valueEscape !== undefined ? where.valueEscape : "\"") + where.value + (where.valueEscape !== undefined ? where.valueEscape : "\""))].join(" "); }).join(" AND "));
 		}
 		else console.error("'wheres' must be a string or an object");
 	}
@@ -65,7 +65,7 @@ function buildSQL_limits(limits)
 	return sql;
 }
 
-function buildSQL(method, from, fields, orders = null, wheres = null, limits = null, options = {})
+function buildSQL(method, tables, fields, orders = null, wheres = null, limits = null, options = {})
 {
 	const escape = "`";
 	const close = ";";
@@ -86,23 +86,73 @@ function buildSQL(method, from, fields, orders = null, wheres = null, limits = n
 				sql.push("DISTINCT");
 			}
 
-			if(typeof fields === "string" && fields === "*")
+			if(options.multiple !== undefined)
 			{
-				if(options.count !== undefined && options.count === true)
+				if(typeof fields === "object")
 				{
-					sql.push("COUNT");
-					sql.push("(" + fields + ")");
+					let temp_fields = [];
+
+					for(let field_index = 0; field_index < fields.length; field_index++)
+					{
+						const table = tables[field_index];
+						const field = fields[field_index];
+
+						if(typeof field === "string")
+						{
+							temp_fields.push(escape + table + "." + field + escape);
+						}
+						else
+						{
+							temp_fields.push(field.map((f) => { return escape + table + "." + f + escape; }).join(", "));
+						}
+					}
+
+					sql.push(temp_fields.join(", "));
 				}
-				else sql.push(fields);
+				else console.error("'fields' must be an array of string or an array of array of string");
 			}
-			else if(typeof fields === "object")
+			else
 			{
-				sql.push(fields.map((field) => { return escape + field + escape; }).join(", "));
+				if(typeof fields === "string" && fields === "*")
+				{
+					if(options.count !== undefined && options.count === true)
+					{
+						sql.push("COUNT");
+						sql.push("(" + fields + ")");
+					}
+					else sql.push(fields);
+				}
+				else if(typeof fields === "object")
+				{
+					sql.push(fields.map((field) => { return escape + field + escape; }).join(", "));
+				}
+				else console.error("'fields' must be a string = to * or an array of string");
 			}
-			else console.error("'fields' must be a string = to * or an array of string");
 
 			sql.push("FROM");
-			sql.push(escape + from + escape);
+
+			if(typeof tables === "string")
+			{
+				sql.push(escape + tables + escape);
+			}
+			else if(typeof tables === "object")
+			{
+				sql.push(tables.map((table) => { return escape + table + escape; }).join(", "));
+			}
+			else console.error("'tables' must be a string or an array of string");
+
+			if(options.multiple !== undefined)
+			{
+				if(wheres === null)wheres = [];
+
+				for(let table_index = 1; table_index < tables.length; table_index++)
+				{
+					const previous_table = tables[table_index - 1];
+					const table = tables[table_index];
+
+					wheres.push({field: previous_table + "." + options.multiple, value: table + "." + options.multiple, operator: "=", valueEscape: escape});
+				}
+			}
 
 			if(wheres !== null) sql = sql.concat(buildSQL_wheres(wheres, escape));
 
@@ -114,7 +164,7 @@ function buildSQL(method, from, fields, orders = null, wheres = null, limits = n
 			sql.push("INSERT");
 			sql.push("INTO");
 
-			sql.push(escape + from + escape);
+			sql.push(escape + tables + escape);
 
 			if(typeof fields === "object" && fields.length === undefined)
 			{
@@ -134,7 +184,7 @@ function buildSQL(method, from, fields, orders = null, wheres = null, limits = n
 		case "UPDATE":
 			sql.push("UPDATE");
 
-			sql.push(escape + from + escape);
+			sql.push(escape + tables + escape);
 
 			sql.push("SET");
 
@@ -156,13 +206,13 @@ function buildSQL(method, from, fields, orders = null, wheres = null, limits = n
 		case "DELETE":
 			sql.push("DELETE");
 			sql.push("FROM");
-			sql.push(escape + from + escape);
+			sql.push(escape + tables + escape);
 
 			if(wheres !== null) sql = sql.concat(buildSQL_wheres(wheres, escape));
 			break;
 		case "TRUNCATE":
-			sql.push(buildSQL("DELETE", from));
-			sql.push(buildSQL("ALTER", from, {field: "AUTO_INCREMENT", value: 1, noEscape: true}));
+			sql.push(buildSQL("DELETE", tables));
+			sql.push(buildSQL("ALTER", tables, {field: "AUTO_INCREMENT", value: 1, noEscape: true}));
 
 			sql.join(" ");
 
@@ -171,7 +221,7 @@ function buildSQL(method, from, fields, orders = null, wheres = null, limits = n
 		case "ALTER":
 			sql.push("ALTER");
 			sql.push("TABLE");
-			sql.push(escape + from + escape);
+			sql.push(escape + tables + escape);
 
 			if(fields.noEscape)
 			{
--- test/SQLTest.js
+++ test/SQLTest.js
@@ -8,142 +8,168 @@ describe("SQL", () =>
 	{
 		describe("SELECT", () => 
 		{
-			it("SELECT * FROM table", () => 
+			it("SELECT * FROM table_test", () => 
 			{
-				const expected_sql = "SELECT * FROM `table`;";
+				const expected_sql = "SELECT * FROM `table_test`;";
 
+				const tables = "table_test";
 				const fields = "*";
-				const sql = buildSQL("SELECT", "table", fields);
+
+				const sql = buildSQL("SELECT", tables, fields);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("SELECT id, pseudo FROM table", () => 
+			it("SELECT id, pseudo FROM table_test", () => 
 			{
-				const expected_sql = "SELECT `id`, `pseudo` FROM `table`;";
+				const expected_sql = "SELECT `id`, `pseudo` FROM `table_test`;";
 
+				const tables = "table_test";
 				const fields = ["id", "pseudo"];
-				const sql = buildSQL("SELECT", "table", fields);
+
+				const sql = buildSQL("SELECT", tables, fields);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("SELECT * FROM table WHERE id = 0", () => 
+			it("SELECT * FROM table_test WHERE id = 0", () => 
 			{
-				const expected_sql = "SELECT * FROM `table` WHERE `id` = \"0\";";
+				const expected_sql = "SELECT * FROM `table_test` WHERE `id` = \"0\";";
 
+				const tables = "table_test";
 				const fields = "*";
 				const wheres = {field: "id", operator: "=", value: "0"};
-				const sql = buildSQL("SELECT", "table", fields, null, wheres);
+
+				const sql = buildSQL("SELECT", tables, fields, null, wheres);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("SELECT * FROM table WHERE id = 0 AND pseudo != \"test\"", () => 
+			it("SELECT * FROM table_test WHERE id = 0 AND pseudo != \"test\"", () => 
 			{
-				const expected_sql = "SELECT * FROM `table` WHERE `id` = 0 AND `pseudo` != \"test\";";
+				const expected_sql = "SELECT * FROM `table_test` WHERE `id` = 0 AND `pseudo` != \"test\";";
 
+				const tables = "table_test";
 				const fields = "*";
 				const wheres = [{field: "id", operator: "=", value: 0}, {field: "pseudo", operator: "!=", value: "test"}];
-				const sql = buildSQL("SELECT", "table", fields, null, wheres);
+
+				const sql = buildSQL("SELECT", tables, fields, null, wheres);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("SELECT * FROM table ORDER BY id ASC", () => 
+			it("SELECT * FROM table_test ORDER BY id ASC", () => 
 			{
-				const expected_sql = "SELECT * FROM `table` ORDER BY `id` ASC;";
+				const expected_sql = "SELECT * FROM `table_test` ORDER BY `id` ASC;";
 
+				const tables = "table_test";
 				const fields = "*";
 				const orders = {field: "id", way: "ASC"};
-				const sql = buildSQL("SELECT", "table", fields, orders);
+
+				const sql = buildSQL("SELECT", tables, fields, orders);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("SELECT * FROM table ORDER BY id ASC, pseudo DESC", () => 
+			it("SELECT * FROM table_test ORDER BY id ASC, pseudo DESC", () => 
 			{
-				const expected_sql = "SELECT * FROM `table` ORDER BY `id` ASC, `pseudo` DESC;";
+				const expected_sql = "SELECT * FROM `table_test` ORDER BY `id` ASC, `pseudo` DESC;";
 
+				const tables = "table_test";
 				const fields = "*";
 				const orders = [{field: "id", way: "ASC"}, {field: "pseudo", way: "DESC"}];
-				const sql = buildSQL("SELECT", "table", fields, orders);
+
+				const sql = buildSQL("SELECT", tables, fields, orders);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("SELECT * FROM table LIMIT 5", () => 
+			it("SELECT * FROM table_test LIMIT 5", () => 
 			{
-				const expected_sql = "SELECT * FROM `table` LIMIT 5;";
+				const expected_sql = "SELECT * FROM `table_test` LIMIT 5;";
 
+				const tables = "table_test";
 				const fields = "*";
 				const limits = 5;
-				const sql = buildSQL("SELECT", "table", fields, null, null, limits);
+
+				const sql = buildSQL("SELECT", tables, fields, null, null, limits);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("SELECT * FROM table LIMIT 10", () => 
+			it("SELECT * FROM table_test LIMIT 10", () => 
 			{
-				const expected_sql = "SELECT * FROM `table` LIMIT 10;";
+				const expected_sql = "SELECT * FROM `table_test` LIMIT 10;";
 
+				const tables = "table_test";
 				const fields = "*";
 				const limits = {limit: 10};
-				const sql = buildSQL("SELECT", "table", fields, null, null, limits);
+
+				const sql = buildSQL("SELECT", tables, fields, null, null, limits);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("SELECT * FROM table LIMIT 10 OFFSET 5", () => 
+			it("SELECT * FROM table_test LIMIT 10 OFFSET 5", () => 
 			{
-				const expected_sql = "SELECT * FROM `table` LIMIT 10 OFFSET 5;";
+				const expected_sql = "SELECT * FROM `table_test` LIMIT 10 OFFSET 5;";
 
+				const tables = "table_test";
 				const fields = "*";
 				const limits = {limit: 10, offset: 5};
-				const sql = buildSQL("SELECT", "table", fields, null, null, limits);
+
+				const sql = buildSQL("SELECT", tables, fields, null, null, limits);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("SELECT country FROM table", () => 
+			it("SELECT country FROM table_test", () => 
 			{
-				const expected_sql = "SELECT `country` FROM `table`;";
+				const expected_sql = "SELECT `country` FROM `table_test`;";
 
+				const tables = "table_test";
 				const fields = ["country"];
-				const sql = buildSQL("SELECT", "table", fields);
+
+				const sql = buildSQL("SELECT", tables, fields);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("SELECT DISTINCT country FROM table", () => 
+			it("SELECT DISTINCT country FROM table_test", () => 
 			{
-				const expected_sql = "SELECT DISTINCT `country` FROM `table`;";
+				const expected_sql = "SELECT DISTINCT `country` FROM `table_test`;";
 
+				const tables = "table_test";
 				const fields = ["country"];
 				const options = {distinct: true};
-				const sql = buildSQL("SELECT", "table", fields, null, null, null, options);
+
+				const sql = buildSQL("SELECT", tables, fields, null, null, null, options);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("SELECT COUNT (*) FROM table", () => 
+			it("SELECT COUNT (*) FROM table_test", () => 
 			{
-				const expected_sql = "SELECT COUNT (*) FROM `table`;";
+				const expected_sql = "SELECT COUNT (*) FROM `table_test`;";
 
+				const tables = "table_test";
 				const fields = "*";
 				const options = {count: true};
-				const sql = buildSQL("SELECT", "table", fields, null, null, null, options);
+
+				const sql = buildSQL("SELECT", tables, fields, null, null, null, options);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("SELECT DISTINCT COUNT (*) FROM table", () => 
+			it("SELECT DISTINCT COUNT (*) FROM table_test", () => 
 			{
-				const expected_sql = "SELECT DISTINCT COUNT (*) FROM `table`;";
+				const expected_sql = "SELECT DISTINCT COUNT (*) FROM `table_test`;";
 
+				const tables = "table_test";
 				const fields = "*";
 				const options = {count: true, distinct: true};
-				const sql = buildSQL("SELECT", "table", fields, null, null, null, options);
+
+				const sql = buildSQL("SELECT", tables, fields, null, null, null, options);
 
 				assert.strictEqual(sql, expected_sql);
 			});
@@ -151,32 +177,38 @@ describe("SQL", () =>
 
 		describe("INSERT", () => 
 		{
-			it("INSERT INTO table (pseudo) VALUES (\"test\")", () => 
+			it("INSERT INTO table_test (pseudo) VALUES (\"test\")", () => 
 			{
-				const expected_sql = "INSERT INTO `table` (`pseudo`) VALUES (\"test\");";
+				const expected_sql = "INSERT INTO `table_test` (`pseudo`) VALUES (\"test\");";
 
+				const tables = "table_test";
 				const fields = {field: "pseudo", value: "test"};
-				const sql = buildSQL("INSERT", "table", fields);
+
+				const sql = buildSQL("INSERT", tables, fields);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("INSERT INTO table (pseudo, activity) VALUES (\"test_pseudo\", \"test_activity\")", () => 
+			it("INSERT INTO table_test (pseudo, activity) VALUES (\"test_pseudo\", \"test_activity\")", () => 
 			{
-				const expected_sql = "INSERT INTO `table` (`pseudo`, `activity`) VALUES (\"test_pseudo\", \"test_activity\");";
+				const expected_sql = "INSERT INTO `table_test` (`pseudo`, `activity`) VALUES (\"test_pseudo\", \"test_activity\");";
 
+				const tables = "table_test";
 				const fields = [{field: "pseudo", value: "test_pseudo"}, {field: "activity", value: "test_activity"}];
-				const sql = buildSQL("INSERT", "table", fields);
+
+				const sql = buildSQL("INSERT", tables, fields);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("INSERT INTO table (pseudo, points) VALUES (\"test_pseudo\", 10)", () => 
+			it("INSERT INTO table_test (pseudo, points) VALUES (\"test_pseudo\", 10)", () => 
 			{
-				const expected_sql = "INSERT INTO `table` (`pseudo`, `points`) VALUES (\"test_pseudo\", 10);";
+				const expected_sql = "INSERT INTO `table_test` (`pseudo`, `points`) VALUES (\"test_pseudo\", 10);";
 
+				const tables = "table_test";
 				const fields = [{field: "pseudo", value: "test_pseudo"}, {field: "points", value: 10}];
-				const sql = buildSQL("INSERT", "table", fields);
+
+				const sql = buildSQL("INSERT", tables, fields);
 
 				assert.strictEqual(sql, expected_sql);
 			});
@@ -184,65 +216,77 @@ describe("SQL", () =>
 
 		describe("UPDATE", () => 
 		{
-			it("UPDATE table SET pseudo = \"test_pseudo\"", () => 
+			it("UPDATE table_test SET pseudo = \"test_pseudo\"", () => 
 			{
-				const expected_sql = "UPDATE `table` SET `pseudo` = \"test_pseudo\";";
+				const expected_sql = "UPDATE `table_test` SET `pseudo` = \"test_pseudo\";";
 
+				const tables = "table_test";
 				const fields = {field: "pseudo", value: "test_pseudo"};
-				const sql = buildSQL("UPDATE", "table", fields);
+
+				const sql = buildSQL("UPDATE", tables, fields);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("UPDATE table SET pseudo = \"test_pseudo\", activity = \"test_activity\"", () => 
+			it("UPDATE table_test SET pseudo = \"test_pseudo\", activity = \"test_activity\"", () => 
 			{
-				const expected_sql = "UPDATE `table` SET `pseudo` = \"test_pseudo\", `activity` = \"test_activity\";";
+				const expected_sql = "UPDATE `table_test` SET `pseudo` = \"test_pseudo\", `activity` = \"test_activity\";";
 
+				const tables = "table_test";
 				const fields = [{field: "pseudo", value: "test_pseudo"}, {field: "activity", value: "test_activity"}];
-				const sql = buildSQL("UPDATE", "table", fields);
+
+				const sql = buildSQL("UPDATE", tables, fields);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("UPDATE table SET pseudo = \"test_pseudo\", points = 10", () => 
+			it("UPDATE table_test SET pseudo = \"test_pseudo\", points = 10", () => 
 			{
-				const expected_sql = "UPDATE `table` SET `pseudo` = \"test_pseudo\", `points` = 10;";
+				const expected_sql = "UPDATE `table_test` SET `pseudo` = \"test_pseudo\", `points` = 10;";
 
+				const tables = "table_test";
 				const fields = [{field: "pseudo", value: "test_pseudo"}, {field: "points", value: 10}];
-				const sql = buildSQL("UPDATE", "table", fields);
+
+				const sql = buildSQL("UPDATE", tables, fields);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("UPDATE table SET activity = \"test_activity\" WHERE pseudo = \"test_pseudo\"", () => 
+			it("UPDATE table_test SET activity = \"test_activity\" WHERE pseudo = \"test_pseudo\"", () => 
 			{
-				const expected_sql = "UPDATE `table` SET `activity` = \"test_activity\" WHERE `pseudo` = \"test_pseudo\";";
+				const expected_sql = "UPDATE `table_test` SET `activity` = \"test_activity\" WHERE `pseudo` = \"test_pseudo\";";
 
+				const tables = "table_test";
 				const fields = {field: "activity", value: "test_activity"};
 				const wheres = {field: "pseudo", operator: "=", value: "test_pseudo"};
-				const sql = buildSQL("UPDATE", "table", fields, null, wheres);
+
+				const sql = buildSQL("UPDATE", tables, fields, null, wheres);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("UPDATE table SET activity = \"test_activity\" WHERE pseudo = \"test_pseudo\" AND line_up != \"test_line_up\"", () => 
+			it("UPDATE table_test SET activity = \"test_activity\" WHERE pseudo = \"test_pseudo\" AND line_up != \"test_line_up\"", () => 
 			{
-				const expected_sql = "UPDATE `table` SET `activity` = \"test_activity\" WHERE `pseudo` = \"test_pseudo\" AND `line_up` != \"test_line_up\";";
+				const expected_sql = "UPDATE `table_test` SET `activity` = \"test_activity\" WHERE `pseudo` = \"test_pseudo\" AND `line_up` != \"test_line_up\";";
 
+				const tables = "table_test";
 				const fields = {field: "activity", value: "test_activity"};
 				const wheres = [{field: "pseudo", operator: "=", value: "test_pseudo"}, {field: "line_up", operator: "!=", value: "test_line_up"}];
-				const sql = buildSQL("UPDATE", "table", fields, null, wheres);
+
+				const sql = buildSQL("UPDATE", tables, fields, null, wheres);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("UPDATE table SET activity = \"test_activity\" WHERE pseudo = \"test_pseudo\" AND id = 5", () => 
+			it("UPDATE table_test SET activity = \"test_activity\" WHERE pseudo = \"test_pseudo\" AND id = 5", () => 
 			{
-				const expected_sql = "UPDATE `table` SET `activity` = \"test_activity\" WHERE `pseudo` = \"test_pseudo\" AND `id` = 5;";
+				const expected_sql = "UPDATE `table_test` SET `activity` = \"test_activity\" WHERE `pseudo` = \"test_pseudo\" AND `id` = 5;";
 
+				const tables = "table_test";
 				const fields = {field: "activity", value: "test_activity"};
 				const wheres = [{field: "pseudo", operator: "=", value: "test_pseudo"}, {field: "id", operator: "=", value: 5}];
-				const sql = buildSQL("UPDATE", "table", fields, null, wheres);
+
+				const sql = buildSQL("UPDATE", tables, fields, null, wheres);
 
 				assert.strictEqual(sql, expected_sql);
 			});
@@ -250,32 +294,38 @@ describe("SQL", () =>
 
 		describe("DELETE", () => 
 		{
-			it("DELETE FROM table WHERE pseudo = test", () => 
+			it("DELETE FROM table_test WHERE pseudo = test", () => 
 			{
-				const expected_sql = "DELETE FROM `table` WHERE `pseudo` = \"test\";";
+				const expected_sql = "DELETE FROM `table_test` WHERE `pseudo` = \"test\";";
 
+				const tables = "table_test";
 				const wheres = {field: "pseudo", operator: "=", value: "test"};
-				const sql = buildSQL("DELETE", "table", null, null, wheres);
+
+				const sql = buildSQL("DELETE", tables, null, null, wheres);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("DELETE FROM table WHERE pseudo = test_pseudo AND activity = test_activity", () => 
+			it("DELETE FROM table_test WHERE pseudo = test_pseudo AND activity = test_activity", () => 
 			{
-				const expected_sql = "DELETE FROM `table` WHERE `pseudo` = \"test_pseudo\" AND `activity` != \"test_activity\";";
+				const expected_sql = "DELETE FROM `table_test` WHERE `pseudo` = \"test_pseudo\" AND `activity` != \"test_activity\";";
 
+				const tables = "table_test";
 				const wheres = [{field: "pseudo", operator: "=", value: "test_pseudo"}, {field: "activity", operator: "!=", value: "test_activity"}];
-				const sql = buildSQL("DELETE", "table", null, null, wheres);
+
+				const sql = buildSQL("DELETE", tables, null, null, wheres);
 
 				assert.strictEqual(sql, expected_sql);
 			});
 
-			it("DELETE FROM table WHERE id = 5", () => 
+			it("DELETE FROM table_test WHERE id = 5", () => 
 			{
-				const expected_sql = "DELETE FROM `table` WHERE `id` = 5;";
+				const expected_sql = "DELETE FROM `table_test` WHERE `id` = 5;";
 
+				const tables = "table_test";
 				const wheres = {field: "id", operator: "=", value: 5};
-				const sql = buildSQL("DELETE", "table", null, null, wheres);
+
+				const sql = buildSQL("DELETE", tables, null, null, wheres);
 
 				assert.strictEqual(sql, expected_sql);
 			});
@@ -287,7 +337,9 @@ describe("SQL", () =>
 			{
 				const expected_sql = "DELETE FROM `table_test`; ALTER TABLE `table_test` AUTO_INCREMENT = 1;";
 
-				const sql = buildSQL("TRUNCATE", "table_test");
+				const tables = "table_test";
+
+				const sql = buildSQL("TRUNCATE", tables);
 
 				assert.strictEqual(sql, expected_sql);
 			});
@@ -299,8 +351,55 @@ describe("SQL", () =>
 			{
 				const expected_sql = "ALTER TABLE `table_test` AUTO_INCREMENT = 1;";
 
+				const tables = "table_test";
 				const fields = {field: "AUTO_INCREMENT", value: 1, noEscape: true};
-				const sql = buildSQL("ALTER", "table_test", fields);
+
+				const sql = buildSQL("ALTER", tables, fields);
+
+				assert.strictEqual(sql, expected_sql);
+			});
+		});
+	});
+
+	describe("multiple", () => 
+	{
+		describe("SELECT", () => 
+		{
+			it("SELECT table1.*, table2.* FROM table1, table2 WHERE table1.id = table2.id", () => 
+			{
+				const expected_sql = "SELECT `table1.*`, `table2.*` FROM `table1`, `table2` WHERE `table1.id` = `table2.id`;";
+
+				const tables = ["table1", "table2"];
+				const fields = ["*", "*"];
+				const options = {multiple: "id"};
+
+				const sql = buildSQL("SELECT", tables, fields, null, null, null, options);
+
+				assert.strictEqual(sql, expected_sql);
+			});
+
+			it("SELECT member.id, member.pseudo, team.name FROM member, team WHERE member.team_id = team.team_id", () => 
+			{
+				const expected_sql = "SELECT `member.id`, `member.pseudo`, `team.name` FROM `member`, `team` WHERE `member.team_id` = `team.team_id`;";
+
+				const tables = ["member", "team"];
+				const fields = [["id", "pseudo"], ["name"]];
+				const options = {multiple: "team_id"};
+
+				const sql = buildSQL("SELECT", tables, fields, null, null, null, options);
+
+				assert.strictEqual(sql, expected_sql);
+			});
+
+			it("SELECT member.id, member.pseudo, team.name, color.code FROM member, team, color WHERE member.team_id = team.team_id AND team.team_id = color.team_id", () => 
+			{
+				const expected_sql = "SELECT `member.id`, `member.pseudo`, `team.name`, `color.code` FROM `member`, `team`, `color` WHERE `member.team_id` = `team.team_id` AND `team.team_id` = `color.team_id`;";
+
+				const tables = ["member", "team", "color"];
+				const fields = [["id", "pseudo"], ["name"], ["code"]];
+				const options = {multiple: "team_id"};
+
+				const sql = buildSQL("SELECT", tables, fields, null, null, null, options);
 
 				assert.strictEqual(sql, expected_sql);
 			});
